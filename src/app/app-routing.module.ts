65a65afae2aecdf2c052085623efca511132efe1
garrisondonl <garrisondl@hotmail.com>
2022-11-29 09:58:54 -0600

    Routes setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CreateComponent } from './create/create.component';
+import { LoginComponent } from './login/login.component';
+import { PostComponent } from './post/post.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'create', component: CreateComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'post', component: PostComponent },
+  { path: 'registration', component: RegistrationComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
