b7cd0605054f685c27ce4fca77223fd7ad02830e
garrisondonl <garrisondl@hotmail.com>
2022-11-28 20:04:41 -0600

    initial project created.  components and bootsrtap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { PostComponent } from './post/post.component';
+import { CreateComponent } from './create/create.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    PostComponent,
+    CreateComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
