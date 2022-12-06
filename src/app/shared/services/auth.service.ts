d585b048c7734bbdb97494bf8a0c192744bfc0fa
kevin-proctor <kevin.proctor26@gmail.com>
2022-12-05 19:50:08 -0600

    first commit

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, NgZone } from '@angular/core';
+import { User } from '../services/user';
+import * as auth from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore, AngularFirestoreDocument, } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  userData: any; // Save logged in user data
+  constructor(
+    public afs: AngularFirestore, // Inject Firestore service
+    public afAuth: AngularFireAuth, // Inject Firebase auth service
+    public router: Router,
+    public ngZone: NgZone // NgZone service to remove outside scope warning
+    ) {
+    /* Saving user data in localstorage when
+    logged in and setting up null when logged out */
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user')!);
+      } else {
+        localStorage.setItem('user', 'null');
+      }
+    })
+  }
+}
