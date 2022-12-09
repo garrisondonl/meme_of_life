7e4f58b9cc1aee3e24ca34892217a54a96270f56
JPH211 <111314566+JPH211@users.noreply.github.com>
2022-12-08 18:21:42 -0600

    final environment ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,27 +3,14 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDinEBLKs2fcVgLT-T1t18M2Xpssfhs7KY",
-  authDomain: "the-memeing-of-life.firebaseapp.com",
-  projectId: "the-memeing-of-life",
-  storageBucket: "the-memeing-of-life.appspot.com",
-  messagingSenderId: "509608043786",
-  appId: "1:509608043786:web:c873a0431ef99a851146f3",
-  measurementId: "G-7P5NWJ3DP4"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 export const environment = {
   production: false,
@@ -46,8 +33,8 @@ export const environment = {
 };
 
 
-  // Usage of api call for giphy
-  /*
+/*
+  // Usage of api for giphy
 
   import { query } from '@angular/animations';
   import { environment } from 'src/environments/environment';
