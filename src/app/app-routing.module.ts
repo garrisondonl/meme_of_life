7f4af229ba1824101dd84f7e62bdaac6bbb10f5f
garrisondonl <garrisondl@hotmail.com>
2022-12-11 21:53:55 -0600

    updated verify EMail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,21 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './login/sign-up/sign-up.component';
 import { PostComponent } from './post/post.component';
 import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './login/verify-email/verify-email.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'create', component: CreateComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent,
+    children: [
+      { path: 'signup', component: SignUpComponent },
+      { path: 'forgotPassword', component: ForgotPasswordComponent},
+      { path: 'verifyEmail', component: VerifyEmailComponent},
+    ]},
   { path: 'post', component: PostComponent },
-  { path: 'signup', component: SignUpComponent },
-  { path: 'forgotPassword', component: ForgotPasswordComponent}
+
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
