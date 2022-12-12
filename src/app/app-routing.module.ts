4daaf4351488ece1696a8d5cd69e4e26a270a753
garrisondonl <garrisondl@hotmail.com>
2022-12-11 20:45:36 -0600

    eleminate registration component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './create/create.component';
 import { LoginComponent } from './login/login.component';
 import { PostComponent } from './post/post.component';
-import { RegistrationComponent } from './registration/registration.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'create', component: CreateComponent },
   { path: 'login', component: LoginComponent },
   { path: 'post', component: PostComponent },
-  { path: 'registration', component: RegistrationComponent },
 ];
 
 @NgModule({
