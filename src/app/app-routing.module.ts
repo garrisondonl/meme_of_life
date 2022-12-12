648bf54dbb12a92383d98999e07e75327e0ee2f3
garrisondonl <garrisondl@hotmail.com>
2022-12-11 20:52:42 -0600

    added sighup and forgotPassword to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CreateComponent } from './create/create.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './login/sign-up/sign-up.component';
 import { PostComponent } from './post/post.component';
+import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'create', component: CreateComponent },
   { path: 'login', component: LoginComponent },
   { path: 'post', component: PostComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'forgotPassword', component: ForgotPasswordComponent}
 ];
 
 @NgModule({
