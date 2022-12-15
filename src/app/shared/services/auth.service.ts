6309de397d97c266ccc25a132b5c16b459c7b201
kevin-proctor <kevin.proctor26@gmail.com>
2022-12-14 21:52:52 -0600

    fixed add to firebase

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -37,7 +37,7 @@ export class AuthService {
         this.SetUserData(result.user);
         this.afAuth.authState.subscribe((user) => {
           if (user) {
-            this.router.navigate(['dashboard']);
+            this.router.navigate(['post']);
           }
         });
       })
@@ -86,7 +86,7 @@ export class AuthService {
   // Sign in with Google
   GoogleAuth() {
     return this.AuthLogin(new auth.GoogleAuthProvider()).then((res: any) => {
-      this.router.navigate(['dashboard']);
+      this.router.navigate(['post']);
     });
   }
   // Auth logic to run auth providers
