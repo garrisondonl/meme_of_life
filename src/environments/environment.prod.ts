8489b20d56ab5d77bd6fc25afeca6e11ba6247d1
garrisondonl <garrisondl@hotmail.com>
2022-12-14 22:26:28 -0600

    deployed to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  production: true,
+  GIPHY_API_URL: "https://api.giphy.com/v1/gifs/search?api_key=GBDcoDZ4L0s78U7Teq7rL1vpb9spD9MK&q=",
+  // GIPHY_API_KEY: "GBDcoDZ4L0s78U7Teq7rL1vpb9spD9MK",
+  // a GIPHY_QUERY string will go inbetween these two so we can add our search term to api call.
+  GIPHY_QUERY_FORMAT: "&limit=10&offset=0&rating=g&lang=en",
+  // general structure: url + '&q=' + query_format
+  GIPHY_API_KEY: "GBDcoDZ4L0s78U7Teq7rL1vpb9spD9MK",
+  // FIREBASE_API_URL: "",
+  // FIREBASE_API_KEY: "",
+  firebase: {
+    apiKey: "AIzaSyDinEBLKs2fcVgLT-T1t18M2Xpssfhs7KY",
+    authDomain: "the-memeing-of-life.firebaseapp.com",
+    projectId: "the-memeing-of-life",
+    storageBucket: "the-memeing-of-life.appspot.com",
+    messagingSenderId: "509608043786",
+    appId: "1:509608043786:web:c873a0431ef99a851146f3",
+    measurementId: "G-7P5NWJ3DP4"
+}
 };
